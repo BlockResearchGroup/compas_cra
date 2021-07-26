f3e85db995c3a2e6b3920fc1f3c9545b329fee0a
GeneKao <kao.gene@gmail.com>
2021-07-26 20:08:33 +0200

    add H model

---
+++ src/compas_cra/equilibrium/cra_penalty_pyomo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Nonlinear formulation with penalty to solve Coupled Rigid-block Equilibrium
+Using Pyomo + IPOPT
+"""
+
+import numpy as np
+import pyomo.environ as pyo
+import time
