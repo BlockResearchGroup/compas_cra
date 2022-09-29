002c92ab0965bbfe4a7d4e068bbbb0c60eb545de
GeneKao <kao.gene@gmail.com>
2022-09-29 09:08:33 +0200

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_cra",
-    version="0.2.1",
+    version="0.2.2",
     description="Coupled Rigid-Block Analysis implementation using COMPAS framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/compas_cra/__init__.py
+++ src/compas_cra/__init__.py
@@ -25,7 +25,7 @@ __author__ = ["Gene Ting-Chun Kao"]
 __copyright__ = "Gene Ting-Chun Kao"
 __license__ = "MIT License"
 __email__ = "kao@arch.ethz.ch, kao.gene@gmail.com"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 
 HERE = os.path.dirname(__file__)
