4c1dd6dc0352c67b5443c340e4d3af87f28bb349
tomvanmele <tom.v.mele@gmail.com>
2024-10-29 21:28:57 +0100

    remove future

--- src/compas_cra/__init__.py
+++ src/compas_cra/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import os
 
 __author__ = ["Gene Ting-Chun Kao"]
--- src/compas_cra/datastructures/__init__.py
+++ src/compas_cra/datastructures/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from .cra_assembly import CRA_Assembly
 
 __all__ = ["CRA_Assembly"]
