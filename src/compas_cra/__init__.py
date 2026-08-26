d4713cc185a29972ba774d1efa85d8d7c7a6725b
tomvanmele <tom.v.mele@gmail.com>
2026-08-26 20:51:36 +0200

    Bump version to 0.6.0

--- src/compas_cra/__init__.py
+++ src/compas_cra/__init__.py
@@ -4,7 +4,7 @@ __author__ = ["Gene Ting-Chun Kao"]
 __copyright__ = "Gene Ting-Chun Kao"
 __license__ = "MIT License"
 __email__ = "kao@arch.ethz.ch, kao.gene@gmail.com"
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 
 HERE = os.path.dirname(__file__)
